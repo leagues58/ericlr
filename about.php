337e7d929fc411110141187f8f5beb8a77a293db
Eric Reeves <leagues58@gmail.com>
2018-11-18 16:01:14 -0500

    Renamed 'Contact' to 'About' and updated navheader to reflect that change.  Added text to About section

--- about.php
+++ about.php
@@ -36,7 +36,20 @@
     <?php require("partials/navheader.php");?>
     <div class="row">
       <div class="12 columns" style="margin-top: 15%">
-        <h4>Contact Page</h4>
+        <h4>About</h4>
+          <p>
+            I firmly hold that you don't really understand something unless you can explain it (well) to other people.  It's for that reason that I decided to make this 
+            website.  This site comprises my attempts at writting down the things I'm learning everyday so that I can share my knowledge with others as well improve
+            my own understanding of the topics I'm discussing.
+          </p>
+          <p>
+            I don't profess to be an expert in anything, far from it.  While I will do my best to ensure that the information on this site is both accurate and, at the very
+             least, not stupid, I don't expect to get everything right.  With any luck, this site will help me get more things right in the future.
+          </p>
+          <hr>
+          <p>
+            You can contact me here:
+          </p>
             <table class="u-full-width">
                 <thead>
                 <tr>
--- partials/navheader.php
+++ partials/navheader.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
     <ul class="navbar-list">
-        <li class="navbar-item"><a class="navbar-link" href="/contact.php">Contact</a></li> 
+        <li class="navbar-item"><a class="navbar-link" href="/about.php">About</a></li> 
         <li class="navbar-item"><a class="navbar-link" href="/projects.php">Projects</a></li>
         <li class="navbar-item"><a class="navbar-link" href="/problems.php">Problems</a></li>
         <li class="navbar-item"><a class="navbar-link" href="/tutorials.php">Tutorials</a></li>
